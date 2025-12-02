b989e9324793d4e563dc573aaf058aa44762a422
Patrik Nazario, PhD. <patriknazario@gmail.com>
2025-12-02 01:57:24 -0300

    Update allowedHosts in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
         allowedHosts: [
-          '65992841-70fd-4281-9f01-10a1ec6e6bf8-00-v3ho78u4irtn.worf.replit.dev',
+          '1a4334bc-8444-4a57-9892-6d73555c09a7-00-37uujw0jit8gy.kirk.replit.dev',
           '.replit.dev',
           '.repl.co',
           'localhost'
